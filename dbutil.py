ee3a9f1383eb907849070ce17e801773c4b4369e
Julien <blondeau.ju@gmail.com>
2013-04-26 17:54:58 +0200

    gestion par thread + bouton annuler

--- dbutil.py
+++ dbutil.py
@@ -12,27 +12,21 @@ port = 3306
 db = MySQLdb.connect(host=host, user=user, passwd=password, port=port)
 
 
-def getDatabasesNamesAndSizes():
+def getDatabaseNamesAndSizes():
     cur = db.cursor()
     cur.execute("SELECT table_schema \"name\", sum( data_length + index_length ) / 1024 / 1024 \"size\" FROM information_schema.TABLES GROUP BY table_schema;")
     result = []
     for row in cur.fetchall():
+        #skip unwanted DB
         if row[0] != 'mysql' and row[0] != 'performance_schema' and row[0] != 'information_schema':
             result.append([row[0], row[1]])
     return result
 
 
 def dumpDatabase(dbName, path):
-    print "dumping " + dbName
     cmd = "mysqldump -u %s --password=%s -h %s --port=%s --lock-tables=false --routines --skip-comments --add-drop-database --databases %s | gzip > \"%s/%s.sql.gz\""
     cmd = cmd % (user, password, host, port, dbName, path, dbName)
     try:
-        retcode = subprocess.call(cmd, shell=True)
-        # if retcode < 0:
-        #     print >>subprocess.sys.stderr, "Child was terminated by signal", -retcode
-        # else:
-        #     print >>subprocess.sys.stderr, "Child returned", retcode
+        subprocess.call(cmd, shell=True)
     except OSError as e:
         print >>subprocess.sys.stderr, "Execution failed:", e
-
-    print "finished :)"
--- pyMySqlDump.py
+++ pyMySqlDump.py
@@ -4,31 +4,43 @@
 import dbutil
 import os
 from time import gmtime, strftime
+from threading import Thread
 
 try:
     import wx
+    import wx.lib.newevent
 except ImportError:
     raise ImportError("The wxPython module is required to run this program")
 
 
-class simpleapp_wx(wx.Frame):
+dumpEvent, EVT_DUMP_EVENT = wx.lib.newevent.NewEvent()
+
+
+def dump_thread(i, basesNb, frame, wx, dbname, path):
+    dbutil.dumpDatabase(dbname, path)
+    evt = dumpEvent(number=i, basesNb=basesNb)
+    wx.PostEvent(frame, evt)
+
+
+class DumpingApp(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title)
-
-        # icon
-        ico = wx.Icon('pyMySqlDump.ico', wx.BITMAP_TYPE_ICO)
-        self.SetIcon(ico)
-
-        self.checkboxes = {}
         self.parent = parent
         self.initialize()
 
     def initialize(self):
+        ico = wx.Icon('pyMySqlDump.ico', wx.BITMAP_TYPE_ICO)
+        self.SetIcon(ico)
+
+        self.checkboxes = {}
+        self.abort = False
+        self.threads = {}
+        self.Bind(EVT_DUMP_EVENT, self.handler_dump_event)
 
         sizer = wx.GridBagSizer()
 
         columnNum = 0
-        for dbinfo in dbutil.getDatabasesNamesAndSizes():
+        for dbinfo in dbutil.getDatabaseNamesAndSizes():
             checkBox = wx.CheckBox(self, -1, label=dbinfo[0]+" ~"+str(int(dbinfo[1]))+" MB")
             checkBox.SetValue(True)
             self.checkboxes[dbinfo[0]] = checkBox
@@ -37,56 +49,63 @@ class simpleapp_wx(wx.Frame):
 
         self.dumpButton = wx.Button(self, -1, label="Dump'em !")
         sizer.Add(self.dumpButton, (columnNum, 1))
-        self.Bind(wx.EVT_BUTTON, self.OnDumpButtonClick, self.dumpButton)
+        self.Bind(wx.EVT_BUTTON, self.on_dumpbutton_click, self.dumpButton)
 
         allButton = wx.Button(self, -1, label="all")
         sizer.Add(allButton, (columnNum, 0))
-        self.Bind(wx.EVT_BUTTON, self.OnAllButtonClick, allButton)
+        self.Bind(wx.EVT_BUTTON, self.on_allbutton_click, allButton)
 
         noneButton = wx.Button(self, -1, label="none")
         sizer.Add(noneButton, (columnNum+1, 0))
-        self.Bind(wx.EVT_BUTTON, self.OnNoneButtonClick, noneButton)
+        self.Bind(wx.EVT_BUTTON, self.on_nonebutton_click, noneButton)
 
         sizer.AddGrowableCol(0)
         self.SetSizerAndFit(sizer)
         self.SetSizeHints(-1, self.GetSize().y, -1, self.GetSize().y)
         self.Show(True)
 
-    def OnDumpButtonClick(self, event):
-        path = self.GetStorePath()
+    def on_dumpbutton_click(self, event):
+
+        if len(self.threads) != 0:
+            #during dump, canceling...
+            self.abort = True
+            return
+
+        counter = 0
+        for dbname in self.checkboxes.iterkeys():
+            if self.checkboxes[dbname].GetValue():
+                counter += 1
+        if counter == 0:
+            #no databases selected
+            return
+
+        path = self.get_store_path()
         if path != '':
             path = path + "/" + strftime("%Y%m%d_%H%M%S", gmtime())
             os.makedirs(path)
+            self.threads = {}
+            howManyBases = counter
 
-            print "dumping into "+path+"..."
-
-            howManyBases = 0
             counter = 0
             for dbname in self.checkboxes.iterkeys():
                 if self.checkboxes[dbname].GetValue():
-                    howManyBases += 1
-
-            for dbname in self.checkboxes.iterkeys():
-                if self.checkboxes[dbname].GetValue():
                     counter += 1
-                    #marche pas, pas de refresh ni update avant le prochain loop de l'appli
-                    #todo : utilisation des threads
-                    self.dumpButton.SetLabel("%s/%s" % (counter, howManyBases))
-                    dbutil.dumpDatabase(dbname, path)
+                    #creates a thread for dumping this DB
+                    self.threads[counter] = Thread(target=dump_thread, args=(counter, howManyBases, self, wx, dbname, path))
 
-            print "Complete !"
-            self.dumpButton.SetLabel("Dump'em !")
-            wx.MessageBox("Complete !", "Info", wx.OK | wx.ICON_INFORMATION)
+            #begin dumping
+            self.threads[1].start()
+            self.dumpButton.SetLabel("1/%s (stop)" % (howManyBases))
 
-    def OnAllButtonClick(self, event):
+    def on_allbutton_click(self, event):
         for dbname in self.checkboxes.iterkeys():
             self.checkboxes[dbname].SetValue(True)
 
-    def OnNoneButtonClick(self, event):
+    def on_nonebutton_click(self, event):
         for dbname in self.checkboxes.iterkeys():
             self.checkboxes[dbname].SetValue(False)
 
-    def GetStorePath(self):
+    def get_store_path(self):
         dirname = ''
         dlg = wx.DirDialog(self, "Where to put ?", "")
         if dlg.ShowModal() == wx.ID_OK:
@@ -94,8 +113,27 @@ class simpleapp_wx(wx.Frame):
         dlg.Destroy()
         return dirname
 
+    def handler_dump_event(self, event):
+        #event handler, every time a DB has been dumped
+        self.dumpButton.SetLabel("%s/%s (stop)" % (event.number+1, event.basesNb))
+        next = event.number+1
+        if next in self.threads.keys() and not self.abort:
+            #more DB to dump
+            self.threads[next].start()
+        elif self.abort:
+            #canceling
+            self.dumpButton.SetLabel("Dump'em !")
+            wx.MessageBox('Canceled', "Info", wx.OK | wx.ICON_EXCLAMATION)
+            self.threads = {}
+            self.abort = False
+        else:
+            #no more DB to dump
+            self.dumpButton.SetLabel("Dump'em !")
+            wx.MessageBox('Complete !', "Info", wx.OK | wx.ICON_INFORMATION)
+            self.threads = {}
+
 
 if __name__ == "__main__":
     app = wx.App()
-    frame = simpleapp_wx(None, -1, 'pyMySqlDump')
+    frame = DumpingApp(None, -1, 'pyMySqlDump')
     app.MainLoop()
